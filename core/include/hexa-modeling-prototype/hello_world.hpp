c0a3f74cae9e639255b475ec0deefe213081cb8d
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2022-03-13 13:12:39 +0100

    Add core subproject

---
+++ core/include/hexa-modeling-prototype/hello_world.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace HMP::HelloWorld
+{
+
+	void launch();
+
+}
---
+++ gui/src/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <hexa-modeling-prototype/hello_world.hpp>
+#include <cstdlib>
+
+int main()
+{
+	HMP::HelloWorld::launch();
+	return EXIT_SUCCESS;
+}
