3cec80c55d4788c5dad9d1a3f63990924e9df3b7
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2022-10-26 11:28:16 +0200

    Implement Actions::TransformVerts

--- core/src/Actions/TransformVerts.cpp
+++ core/src/Actions/TransformVerts.cpp
@@ -6,15 +6,45 @@
 #include <HMP/Meshing/Utils.hpp>
 #include <algorithm>
 #include <utility>
+#include <stdexcept>
 
 namespace HMP::Actions
 {
 
+	TransformVerts::Vert::Vert(Dag::Element& _element, Id _vertOffset) : m_element{ &_element }, m_vertOffset{ _vertOffset }
+	{
+		if (m_vertOffset >= 8)
+		{
+			throw std::domain_error{ "bad vertex offset" };
+		}
+	}
+
+	Dag::Element& TransformVerts::Vert::element()
+	{
+		return *m_element;
+	}
+
+	const Dag::Element& TransformVerts::Vert::element() const
+	{
+		return *m_element;
+	}
+
+	Id TransformVerts::Vert::vertOffset() const
+	{
+		return m_vertOffset;
+	}
+
 	void TransformVerts::apply(const Mat4& _transform)
 	{
-		Dag::Utils::transform(*root(), _transform);
-		Meshing::Utils::addLeafs(mesher(), *root(), true);
-		mesher().updateMesh();
+		Meshing::Mesher& mesher{ this->mesher() };
+		const Meshing::Mesher::Mesh& mesh{ mesher.mesh() };
+		for (Vert& vert : m_verts)
+		{
+			const Id pid{ mesher.elementToPid(vert.element()) };
+			const Id vid{ mesh.poly_vert_id(pid, vert.vertOffset()) };
+			mesher.moveVert(vid, _transform * mesh.vert(vid));
+		}
+		mesher.updateMesh();
 	}
 
 	void TransformVerts::apply()
@@ -26,6 +56,22 @@ namespace HMP::Actions
 	{
 		apply(m_transform.inverse());
 	}
+
+	TransformVerts::TransformVerts(const Mat4& _transform, const std::vector<Vert> _verts)
+		: m_transform{ _transform }, m_verts{ _verts }
+	{}
+
+	TransformVerts::TransformVerts(Mat4&& _transform, const std::vector<Vert> _verts)
+		: m_transform{ std::move(_transform) }, m_verts{ _verts }
+	{}
+
+	TransformVerts::TransformVerts(const Mat4& _transform, std::vector<Vert>&& _verts)
+		: m_transform{ _transform }, m_verts{ std::move(_verts) }
+	{}
+
+	TransformVerts::TransformVerts(Mat4&& _transform, std::vector<Vert>&& _verts)
+		: m_transform{ std::move(_transform) }, m_verts{ std::move(_verts) }
+	{}
 
 	const Mat4& TransformVerts::transform() const
 	{
