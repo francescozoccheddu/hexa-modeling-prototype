12684509967b956bb393d627af02d8af2bf16acc
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2022-12-12 20:22:22 +0100

    Minor refactor

---
+++ core/src/Projection/jacobianAdvance.cpp
@@ -0,0 +1,14 @@
+#include <HMP/Projection/jacobianAdvance.hpp>
+
+#include <algorithm>
+#include <limits>
+#include <cinolib/parallel_for.h>
+
+namespace HMP::Projection
+{
+
+    static constexpr unsigned int c_minVertsForParallelFor{ 1024 };
+
+
+
+}
