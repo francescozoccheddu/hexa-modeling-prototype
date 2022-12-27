420e5387208d275998b052aef88ff011962792bc
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2022-12-27 19:39:40 +0100

    Fix Actions::Project

--- core/src/Actions/Project.cpp
+++ core/src/Actions/Project.cpp
@@ -7,34 +7,18 @@ namespace HMP::Actions
 
 	void Project::apply()
 	{
-		/*Meshing::Mesher& mesher{ this->mesher() };
-		const Meshing::Mesher::Mesh& source{ mesher.mesh() };
 		if (!m_prepared)
 		{
 			m_prepared = true;
-			const std::vector<Vec> newVerts{ Projection::project(source, m_target, m_pointFeats, m_pathFeats, m_options) };
-			m_vertMoves.resize(newVerts.size());
-			for (I sourceVi{}; sourceVi < newVerts.size(); sourceVi++)
-			{
-				const Id sourceVid{ toId(sourceVi) };
-				const Id sourcePid{ source.adj_v2p(sourceVid).front() };
-				const Id sourceVertOffset{ source.poly_vert_offset(sourcePid, sourceVid) };
-				m_vertMoves[sourceVi] = VertMove{
-					.position = newVerts[sourceVi],
-					.vertOffset = sourceVertOffset,
-					.element = &mesher.pidToElement(sourcePid)
-				};
-			}
+			m_otherVerts = Projection::project(mesher().mesh(), m_target, m_pointFeats, m_pathFeats, m_options);
 		}
-		for (VertMove& move : m_vertMoves)
+		std::vector<Vec> oldVerts{ mesher().mesh().vector_verts() };
+		for (I vi{}; vi < m_otherVerts.size(); vi++)
 		{
-			const Id pid{ mesher.elementToPid(*move.element) };
-			const Id vid{ source.poly_vert_id(pid, move.vertOffset) };
-			const Vec oldPosition{ source.vert(vid) };
-			mesher.moveVert(vid, move.position);
-			move.position = oldPosition;
+			mesher().moveVert(toId(vi), m_otherVerts[vi]);
 		}
-		mesher.updateMesh();*/
+		m_otherVerts.swap(oldVerts);
+		mesher().updateMesh();
 	}
 
 	void Project::unapply()
