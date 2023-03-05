45725e7b8b82af511dd46b7ef43be174a91281e7
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2023-03-05 12:55:44 +0100

    Add sharp features support in altProject

--- core/src/Projection/altProject.cpp
+++ core/src/Projection/altProject.cpp
@@ -10,6 +10,7 @@
 #include <utility>
 #include <cpputils/range/of.hpp>
 #include <cpputils/range/index.hpp>
+#include <HMP/Projection/Utils.hpp>
 
 namespace HMP::Projection
 {
@@ -75,21 +76,57 @@ namespace HMP::Projection
         };
     }
 
+    utilities::CurveMesh toAltCurveMesh(const cinolib::AbstractPolygonMesh<>& _target)
+    {
+        std::vector<std::array<int, 3>> tris;
+        tris.reserve(toI(_target.num_polys()) * 3);
+        for (Id pid{}; pid < _target.num_polys(); pid++)
+        {
+            const std::vector<Id>& vids{_target.poly_tessellation(pid)};
+            for (I t{}; t < vids.size() / 3; t++)
+            {
+                tris.push_back({
+                    static_cast<int>(vids[3 * t + 0]),
+                    static_cast<int>(vids[3 * t + 1]),
+                    static_cast<int>(vids[3 * t + 2])
+                    });
+            }
+        }
+        return {
+            ._pts{cpputils::range::of(_target.vector_verts()).map(&toAltVec).toVector()},
+            ._edges{cpputils::range::count(_target.num_edges()).map([&](const Id _eid) {
+                return std::array<int, 2>{
+                    static_cast<int>(_target.edge_vert_id(_eid, 0)),
+                    static_cast<int>(_target.edge_vert_id(_eid, 1)),
+                };
+            }).toVector()}
+        };
+    }
+
     // --------------------------------------
 
     std::vector<Vec> altProject(const Meshing::Mesher::Mesh& _source, const cinolib::AbstractPolygonMesh<>& _target, const std::vector<Utils::Point>& _pointFeats, const std::vector<Utils::EidsPath>& _pathFeats, const Options& _options)
     {
         assert(_options.alternativeMethod);
-        utilities::HexahedralMesh altSource{toAltHexMesh(_source)};
-        const utilities::TriangleMesh altTarget{toAltTriMesh(_target)};
-        const utilities::CurveMesh altTargetCurves{};
 
+        // source
+        utilities::HexahedralMesh altSource{toAltHexMesh(_source)};
         vertex_smoother smoother(altSource);
+
+        // no locks
         const std::vector<bool> locks(altSource._pts.size(), false);
-        const std::vector<int> allTris{cpputils::range::count(static_cast<int>(altTarget._tris.size())).toVector()};
         smoother.set_locked_vertices(locks);
-        smoother.set_bnd_triangles(altTarget);
-        smoother.set_features_segment(altTargetCurves);
+
+        // target surface
+        const utilities::TriangleMesh altTargetSurf{toAltTriMesh(_target)};
+        smoother.set_bnd_triangles(altTargetSurf);
+
+        // target edges
+        const utilities::CurveMesh altTargetEdges{toAltCurveMesh(_target)};
+        smoother.set_features_segment(altTargetEdges);
+
+        // vert to surf mapping
+        const std::vector<int> allTris{cpputils::range::count(static_cast<int>(altTargetSurf._tris.size())).toVector()};
         for (Id vid{}; vid < _source.num_verts(); vid++)
         {
             if (_source.vert_is_on_srf(vid) && _source.vert_is_visible(vid))
@@ -98,14 +135,21 @@ namespace HMP::Projection
             }
         }
 
-        /*
-        for each vertex v in altSource {
-            if v is in volume : continue;
-            if v is CAD corner : smoother.set_vertex_point(v, coordinate);
-            if v is on CAD curve : smoother.set_vertex_segments(v, listOfEdgesTheVertexCouldBeOn); // on altTargetCurves
-            if v is on CAD surface : smoother.set_vertex_triangles(v, listOfTriangleTheVertexCouldBeOn); // on altTarget
+        // vert to edge chain mapping (feature paths)
+        for (const Utils::EidsPath& path : _pathFeats)
+        {
+            const std::vector<int> edges{ cpputils::range::of(path.targetEids).cast<int>().toVector() };
+            for (const Id vid : Utils::eidsToVidsPath(_source, path.sourceEids))
+            {
+                smoother.set_vertex_segments(static_cast<int>(vid), edges);
+            }
         }
-        */
+
+        // vert to vert mapping (feature points)
+        for (const Utils::Point& point : _pointFeats)
+        {
+            smoother.set_vertex_point(static_cast<int>(point.sourceVid), toAltVec(_target.vert(point.targetVid)));
+        }
 
         smoother.execute(static_cast<unsigned int>(_options.iterations));
 
